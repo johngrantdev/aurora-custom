bcba8e5b933c91a4b70c3546c406e9cc7bdc06b9
John Grant <john@johngrant.dev>
2026-06-17 13:39:35 +1000

    remove looking glass from build for now due to no looking glass fedora 44 builds yet

--- build_files/desktop.sh
+++ build_files/desktop.sh
@@ -3,29 +3,29 @@ set -eoux pipefail
 
 ### Looking Glass
 
-# looking-glass-client binary
-# Verify COPR is still current: https://copr.fedorainfracloud.org/coprs/sentry/looking-glass/
-dnf5 -y copr enable sentry/looking-glass
-dnf5 install -y looking-glass-client
-
-# kvmfr kernel module (shared memory transport between host and VM)
-# Verify COPR is still current: https://copr.fedorainfracloud.org/coprs/hikariknight/looking-glass-kvmfr/
-dnf5 -y copr enable hikariknight/looking-glass-kvmfr
-dnf5 install -y akmods akmod-kvmfr kernel-devel
+# # looking-glass-client binary
+# # Verify COPR is still current: https://copr.fedorainfracloud.org/coprs/sentry/looking-glass/
+# dnf5 -y copr enable sentry/looking-glass
+# dnf5 install -y looking-glass-client
+# 
+# # kvmfr kernel module (shared memory transport between host and VM)
+# # Verify COPR is still current: https://copr.fedorainfracloud.org/coprs/hikariknight/looking-glass-kvmfr/
+# dnf5 -y copr enable hikariknight/looking-glass-kvmfr
+# dnf5 install -y akmods akmod-kvmfr kernel-devel
 
 KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-core)"
-akmods --force --kernels "$KVER"
-modinfo "/usr/lib/modules/${KVER}/extra/kvmfr/kvmfr.ko" || \
-    modinfo "/usr/lib/modules/${KVER}/extra/kvmfr/kvmfr.ko.xz"
-
-# Allow the kvm group to access the kvmfr device (user must be in the kvm group)
-install -Dm644 /dev/null /usr/lib/udev/rules.d/99-kvmfr.rules
-printf 'SUBSYSTEM=="kvmfr", OWNER="root", GROUP="kvm", MODE="0660"\n' \
-    > /usr/lib/udev/rules.d/99-kvmfr.rules
-
-# Disable COPRs so they don't end up enabled on the final image
-dnf5 -y copr disable sentry/looking-glass
-dnf5 -y copr disable hikariknight/looking-glass-kvmfr
+# akmods --force --kernels "$KVER"
+# modinfo "/usr/lib/modules/${KVER}/extra/kvmfr/kvmfr.ko" || \
+#     modinfo "/usr/lib/modules/${KVER}/extra/kvmfr/kvmfr.ko.xz"
+# 
+# # Allow the kvm group to access the kvmfr device (user must be in the kvm group)
+# install -Dm644 /dev/null /usr/lib/udev/rules.d/99-kvmfr.rules
+# printf 'SUBSYSTEM=="kvmfr", OWNER="root", GROUP="kvm", MODE="0660"\n' \
+#     > /usr/lib/udev/rules.d/99-kvmfr.rules
+# 
+# # Disable COPRs so they don't end up enabled on the final image
+# dnf5 -y copr disable sentry/looking-glass
+# dnf5 -y copr disable hikariknight/looking-glass-kvmfr
 
 ### VFIO
 systemctl enable vfio-rebind-gpu-usb.service
